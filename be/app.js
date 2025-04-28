8b5da63ef6a4a508d8c8281e0be18f184743dedd
Nikola <nzencic@gmail.com>
2025-04-28 13:00:13 +0200

    :Deploy

--- be/app.js
+++ be/app.js
@@ -39,7 +39,7 @@ app.use(
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://where-s-waldo-tau.vercel.app",
     credentials: true,
     optionsSuccessStatus: 200,
     allowedHeaders: ["Content-Type"],
