29f7c5dbfbe59abad5c4db549f605f5be214179d
Nikola <nzencic@gmail.com>
2025-04-28 14:00:27 +0200

    fix

--- be/app.js
+++ be/app.js
@@ -17,13 +17,6 @@ const databaseUrl =
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL;
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-});
 
 
 
